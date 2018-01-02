7c93cb625ae289f41b0b95e5e38525cc3c1679f1
Dale Schumacher <dale.schumacher@gmail.com>
2018-01-02 09:56:36 -0600

    2018 copyright

--- VO.js
+++ VO.js
@@ -4,7 +4,7 @@ VO.js - "crlf/VO" JSON Value Objects
 
 The MIT License (MIT)
 
-Copyright (c) 2017 Dale Schumacher
+Copyright (c) 2017-2018 Dale Schumacher
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
