5faceba88c8bb8590b8b11a951bedcc78d49c3e8
Dale Schumacher <dalnefre@yahoo.com>
2019-10-24 22:03:26 -0600

    expand expression vocabulary

--- abcm/abcm.h
+++ abcm/abcm.h
@@ -26,6 +26,8 @@ extern BYTE s_behavior[];
 extern BYTE s_name[];
 extern BYTE s_value[];
 extern BYTE s_type[];
+extern BYTE s_in[];
+extern BYTE s_with[];
 extern BYTE s_const[];
 extern BYTE s_level[];
 extern BYTE s_error[];
@@ -36,12 +38,17 @@ extern BYTE k_actor_become[];
 extern BYTE k_actor_ignore[];
 extern BYTE k_actor_assign[];
 extern BYTE k_actor_fail[];
-extern BYTE k_log_print[];  // --DEPRECATED--
 extern BYTE k_actor_behavior[];
 extern BYTE k_actor_create[];
-extern BYTE k_actor_state[];
+extern BYTE k_actor_message[];
 extern BYTE k_actor_self[];
+extern BYTE k_actor_has_state[];
+extern BYTE k_actor_state[];
+extern BYTE k_dict_has[];
+extern BYTE k_dict_get[];
+extern BYTE k_dict_bind[];
 extern BYTE k_expr_literal[];
+extern BYTE k_log_print[];  // --DEPRECATED--
 
 int start_abcm();  // ok == 0, fail != 0
 
